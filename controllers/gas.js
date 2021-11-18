3b175da9f8ba9f10be45c56808dea02302d11381
hemanth97 <hsai7980@gmail.com>
2021-11-17 22:57:24 -0600

    added

--- controllers/gas.js
+++ controllers/gas.js
@@ -44,7 +44,7 @@ exports.gas_create_post = async function (req, res) {
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    // {"gas_type":"goat", "cost":12, "size":"large"} 
     document.name = req.body.name;
     document.smell = req.body.smell;
     document.weight = req.body.weight;
@@ -112,6 +112,36 @@ exports.gas_create_Page = function (req, res) {
     console.log("create view")
     try {
         res.render('gascreate', { title: 'gas Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a gas. 
+// query provides the id 
+exports.gas_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await gas.findById(req.query.id)
+        res.render('gasupdate', { title: 'gas Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query 
+exports.gas_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await gas.findById(req.query.id)
+        res.render('gasdelete', {
+            title: 'gas Delete', toShow:
+                result
+        });
     }
     catch (err) {
         res.status(500)
--- routes/gas.js
+++ routes/gas.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-const gas_controlers= require('../controllers/gas');
+const gas_controlers = require('../controllers/gas');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('gas', { title: 'Search Results gas' });
 });
 
-/* GET detail gas page */ 
-router.get('/detail', gas_controlers.gas_view_one_Page); 
+/* GET detail gas page */
+router.get('/detail', gas_controlers.gas_view_one_Page);
+
+/* GET create gas page */
+router.get('/create', gas_controlers.gas_create_Page);
+
+/* GET create update page */
+router.get('/update', gas_controlers.gas_update_Page);
 
 /* GET create gas page */ 
-router.get('/create', gas_controlers.gas_create_Page); 
+router.get('/delete', gas_controlers.gas_delete_Page);  
 
 module.exports = router;
