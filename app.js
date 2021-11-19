7b6fe44d7c5f217eb4d118c27fc5ab8c9d0efbef
hemanth97 <hsai7980@gmail.com>
2021-11-19 01:57:33 -0600

    Created new account and edited gas.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
+
 const connectionString = 'mongodb+srv://hemanthvelamuri:Hemanth%4097@cluster0.zyjcx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose = require('mongoose');
 mongoose.connect(connectionString,
@@ -63,6 +66,33 @@ async function recreateDB() {
 
 let reseed = true;
 if (reseed) { recreateDB(); }
+
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } ))
+
+  app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/gas.js
+++ routes/gas.js
@@ -4,13 +4,16 @@ var router = express.Router();
 const gas_controlers = require('../controllers/gas');
 
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('gas', { title: 'Search Results gas' });
-});
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   res.render('gas', { title: 'Search Results gas' });
+// });
 
 /* GET detail gas page */
 router.get('/detail', gas_controlers.gas_view_one_Page);
+
+
+router.get('/', gas_controlers.gas_view_all_Page);
 
 /* GET create gas page */
 router.get('/create', gas_controlers.gas_create_Page);
